36232a5a41665f5933567e1c884919c7d6676928
amelia-liston <aliston@bu.edu>
2024-10-07 23:04:00 -0400

    Final changes, ready for submission

--- HW3-Design/wildlife_tracker/animal_management/animal.py
+++ HW3-Design/wildlife_tracker/animal_management/animal.py
@@ -5,9 +5,9 @@ class Animal:
         
     def __init__(self,
             animal_id: int,
+            species: str,
             age: Optional[int] = None,
-            health_status: Optional[str] = None,
-            species: str) -> None:
+            health_status: Optional[str] = None,) -> None:
         self.animal_id = animal_id
         self.age = age
         self.health_status = health_status
